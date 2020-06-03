e921741fd69e2ba27070fe127c8beb1f48f4450b
Tobias <tobias@tim.pe>
2020-06-03 08:48:56 +0200

    Added automated polling via setTimeout

--- index.js
+++ index.js
@@ -18,9 +18,9 @@ class LaundrifySensor {
         this.id = config['id'];
         this.ipAddress = config['ipAddress'];
         this.threshold = parseInt(config['threshold']);
-
+        this.interval = parseInt(config['interval']);
         this.enabledServices = [];
-
+        this.timer = null;
 
         this.initSensor();
     }
@@ -32,15 +32,15 @@ class LaundrifySensor {
 
         this.pollStatus(() => {
 
-         this.informationService = new Service.AccessoryInformation();
-        this.informationService
-        .setCharacteristic(Characteristic.Manufacturer, 'laundrify')
-        .setCharacteristic(Characteristic.Model, 'laundrify')
-        .setCharacteristic(Characteristic.SerialNumber, this.deviceId)
-        .setCharacteristic(Characteristic.FirmwareRevision, this.version);
+            this.informationService = new Service.AccessoryInformation();
+            this.informationService
+            .setCharacteristic(Characteristic.Manufacturer, 'laundrify')
+            .setCharacteristic(Characteristic.Model, 'laundrify')
+            .setCharacteristic(Characteristic.SerialNumber, this.deviceId)
+            .setCharacteristic(Characteristic.FirmwareRevision, this.version);
 
-        this.enabledServices.push(this.informationService);
-        this.log('Got information service');
+            this.enabledServices.push(this.informationService);
+            this.log('Got information service');
         // fan service
         this.sensorService = new Service.ContactSensor(this.name);
         this.sensorService
@@ -48,45 +48,46 @@ class LaundrifySensor {
         .on('get', this.getSensorState.bind(this));
         this.enabledServices.push(this.sensorService);
 
- 
-        });
- }
-       
+
+    });
+        this.timer = setInterval(this.pollStatus, interval)
+    }
 
 
-        pollStatus(callback) {
-            const status_url = 'http://' + this.ip + '/status';
 
-            const req = http.get(status_url, res => {
-                let data = '';
-                res.on('data', (chunk) => {
-                    data += data;
-                });
-                res.on('end', () => {
-                    var statusObject = JSON.parse(data);
-                    this.deviceId = statusObject.deviceId;
-                    this.power = statusObject.power;
-                    this.version = statusObject.version;
-                    callback();
-                });
-            }).on("error", (err) => {
-                this.log('error on getData');
+    pollStatus(callback) {
+        const status_url = 'http://' + this.ip + '/status';
+
+        const req = http.get(status_url, res => {
+            let data = '';
+            res.on('data', (chunk) => {
+                data += data;
             });
-        }
-
-
-
-
-
-        getSensorState(callback) {
-            this.pollStatus(() => {
-                if (this.power >= this.threshold) {
-                    callback(null, true);
-                } else {
-                    callback(null, false);
+            res.on('end', () => {
+                var statusObject = JSON.parse(data);
+                this.deviceId = statusObject.deviceId;
+                this.power = statusObject.power;
+                this.version = statusObject.version;
+                if (callback != undefined) {
+                    callback();
                 }
-            })
+            });
+        }).on("error", (err) => {
+            this.log('error on pollStatus');
+        });
+    }
+
+
+
+
+
+    getSensorState(callback) {
+        if (this.power >= this.threshold) {
+            callback(null, true);
+        } else {
+            callback(null, false);
         }
+    }
 
     
     getServices() {
